16ce48c975e03c5ad638628fc1b45033b47eb2ea
saweedkh <saweedkh@gmail.com>
2023-11-02 11:11:33 +0330

    create products app

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Product
+
 # Register your models here.
+
+@admin.register(Product)
+class ProDuctAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'active')
--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -12,3 +13,12 @@ class Product(models.Model):
 
     datetime_dreated = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self) -> str:
+        return self.title 
+    
+    def get_absolute_url(self):
+        return reverse("product_detail", kwargs={"pk": self.pk})
+    
